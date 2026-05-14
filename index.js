08f437a41f546695c69af3e400015948f908ea14
akbarPaluGada <akbariridha@gmail.com>
2026-05-14 10:26:45 +0800

    Install dependency "npm install mysql2"

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ dotenv.config();
 const bot = new Bot(process.env.BOT_TOKEN);
 
 // kalau ada pesan
-bot.on("message:text", (ctx) => {
-  ctx.reply("Halo! Kamu bilang: " + ctx.message.text);
-});
+// bot.on("message:text", (ctx) => {
+//   ctx.reply("Halo! Kamu bilang: " + ctx.message.text);
+// });
 
 // command /start
 bot.command("start", (ctx) => {
   ctx.reply("Bot aktif 🚀");
 });
+
+// bot.on("message:text")      // pesan teks
+// bot.on("message:photo")     // gambar
+// bot.on("message:video")     // video
+// bot.on("message:audio")     // audio
+// bot.on("message:document")  // file
 
 bot.start();
 
