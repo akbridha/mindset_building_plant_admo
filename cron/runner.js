f77ecc28bef35444c2711307d24afa37d7c7d894
akbarPaluGada <akbariridha@gmail.com>
2026-05-19 09:22:37 +0800

    Berhasil mengirimkan pesan reminder sesuai jadwal reminder kepada orang yang tepat.

--- cron/runner.js
+++ cron/runner.js
@@ -2,26 +2,58 @@ const {
   checkReminderService,
   checkDataCheckpointTime
 } = require("../services/check_reminder_service");
+const { Bot } = require("grammy");
+
+const stateService = require("../services/stateService");
+
+console
+
 async function startCron() {
+  const bot = new Bot((process.env.BOT_TOKEN));
   console.log("Cron started");
 
   const timeRange = 1; // in minutes
 
   // run immediately once
-  const allScheduleInRange =   await checkReminderService(timeRange);
-
-  const reminderInRange =    checkDataCheckpointTime(allScheduleInRange);
-
-  console.log("reminderInRange", reminderInRange);
-
+  
+  
+  // async function processReminders(range) {
+  // }
 
 
 
-  // run every minute
+  
+  
+  
+  // await processReminders(timeRange);
+  
+  // const reminderInRange =    checkDataCheckpointTime(allScheduleInRange);
+  
+  
   setInterval(async () => {
-    await checkReminderService();
-//   }, 60 * 1000);
-  }, 3 * 1000);
+    
+    const allScheduleInRange =   await checkReminderService(timeRange);
+    if(!allScheduleInRange || allScheduleInRange.length === 0) {
+      console.log("No reminders to process.");
+      return;
+    }
+    
+    for (const reminder of allScheduleInRange) {
+
+      // sendM
+      console.log({
+        telegram_id: reminder.telegram_id,
+        checkpoint_time: reminder.checkpoint_time,
+        task_description: reminder.task_description,
+      });
+          await bot.api.sendMessage( reminder.telegram_id,reminder.task_description);
+          await stateService.setState(reminder.telegram_id, "awaiting_checkpoint_response", { });
+    }
+  // run every minute
+    // await checkReminderService();
+  }, 60 * 1000);
+
+
 }
 
 module.exports = {
