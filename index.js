04060c56ab94169444a566dcda9da0e0ddf58e7d
akbarPaluGada <akbariridha@gmail.com>
2026-05-26 15:30:28 +0800

    Berhasil Membuat Generate ref dengan QR.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cron = require("node-cron");
 const dotenv = require("dotenv");
 const { startCron } = require("./cron/runner");
 
+const QRCode = require("qrcode");
+const { InputFile } = require("grammy");
+
 dotenv.config();
 
 const bot = new Bot(process.env.BOT_TOKEN);
@@ -68,6 +71,20 @@ bot.command("list_task", listTaskCommand);
 bot.command("add_task", addTaskCommand);
 bot.command("remove_task", removeTaskCommand);
 bot.command("cancel", cancelCommand);
+
+
+
+
+bot.command("qr", async (ctx) => {
+  const text = ctx.match || "default data dari backend";
+
+  const buffer = await QRCode.toBuffer(text);
+
+  await ctx.replyWithPhoto(new InputFile(buffer, "qr.png"), {
+    caption: "Ini QR code kamu",
+  });
+});
+
 
 // ========== HANDLE CALLBACK QUERIES (Button Clicks) ==========
 bot.on("callback_query:data", async (ctx) => {
