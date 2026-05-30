1c4e0f9cb368a1a36b5e8607abd9f9e6e3e9b6e7
akbarPaluGada <akbariridha@gmail.com>
2026-05-31 07:38:09 +0800

    Coba generate fitur pecobaan pertama.

--- middleware/messageRouter.js
+++ middleware/messageRouter.js
@@ -1,6 +1,6 @@
 const { addTaskStep2, addTaskStep3, addTaskStep4bInterval, addTaskStep5, setExtendedTarget } = require("../commands/add_task");
 const { removeTaskStep2, removeTaskConfirm } = require("../commands/remove_task");
-const {createProgress, doneTask, confirmProgress, updateProgress} = require("../commands/progress_task");
+const {createProgress, doneTask, confirmProgress, updateProgress, askProgressNote} = require("../commands/progress_task");
 const {processUserInput} = require("../commands/set_user_reminder_time");
 const {descriptionQuestion, targetDescriptionQuestion, proceedTaskEdit} = require("../commands/edit_task");
 
@@ -60,6 +60,9 @@ async function messageRouter(ctx) {
         
       case "awaiting_update_response":
         return updateProgress(ctx, userInput);
+
+      case "awaiting_progress_note":
+        return askProgressNote(ctx, userInput);
 
       case "awaiting_reminder_time":
         return processUserInput(ctx, userInput);
